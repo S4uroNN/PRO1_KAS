0cb01d992d4241e977ede45b0ae3025c7f877a2c
S4uroNN <alin_constantin15@hotmail.com>
2021-11-12 11:01:53 +0100

    add modelklasser

--- Konference/src/application/model/Deltager.java
+++ Konference/src/application/model/Deltager.java
@@ -1,4 +1,10 @@
 package application.model;
 
 public class Deltager extends Person{
+    private boolean foredragsholder;
+
+    public Deltager(String name, int age, boolean foredragsholder){
+        super(name, age);
+        this.foredragsholder = foredragsholder;
+    }
 }
--- Konference/src/application/model/Ledsager.java
+++ Konference/src/application/model/Ledsager.java
@@ -1,4 +1,10 @@
 package application.model;
 
 public class Ledsager extends Person{
+    private boolean ledsager;
+
+    public Ledsager(String name, int age, boolean ledsager){
+        super(name, age);
+        this.ledsager = ledsager;
+    }
 }
